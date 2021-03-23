38b217defcb6cacd361d5788d06725d2f77825d5
root <root@iqbal.heymnpl2vh0ujozbrymtoeynvb.cwx.internal.cloudapp.net>
2021-03-23 04:30:39 +0000

    derp

--- derp_whyred.mk
+++ derp_whyred.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Enable updating of APEXes
-(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
