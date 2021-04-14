f55786db28cfed485cab795a142c1d261b00449c
predator112 <mi560334@gmail.com>
2021-04-14 00:33:26 +0000

    ssos ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_whyred.mk
+    $(LOCAL_DIR)/ssos_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_whyred-user \
-    derp_whyred-userdebug \
-    derp_whyred-eng
+    ssos_whyred-user \
+    ssos_whyred-userdebug \
+    ssos_whyred-eng
--- ssos_whyred.mk
+++ ssos_whyred.mk
@@ -23,12 +23,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
+USE_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 IS_PHONE := true
-DERP_BUILDTYPE := Unofficial
+SSOS_BUILDTYPE := Unofficial
+
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.ssos.cpu=SDM636
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -39,7 +43,7 @@ $(call inherit-product, vendor/MiuiCamera/config.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := derp_whyred
+PRODUCT_NAME := ssos_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -47,6 +51,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210305.006 7119741 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
